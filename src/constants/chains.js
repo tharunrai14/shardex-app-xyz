44d2478bd545392bb772aad6ae28205f3d228e2b
tharunrai14 <67855850+tharunrai14@users.noreply.github.com>
2022-06-07 22:46:25 +0530

    Update chains.js

--- src/constants/chains.js
+++ src/constants/chains.js
@@ -1,14 +1,14 @@
-export const networks = [3,4]
+export const networks = [8080 ,4 ]
 
 export const ChainId = {
-   MAINNET: 1,
+   MAINNET: 8080,
  
   RINKEBY: 4,
  
 }
 
 export const routerAddress = new Map();
- routerAddress.set(ChainId.MAINNET, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+ routerAddress.set(ChainId.MAINNET, "0xdc1bc88bf88ac25be118d705c78ebf5d8c45914d");
 
 routerAddress.set(ChainId.RINKEBY, "0x14e4DAe87990FC8947718f7ac0317CdD9b58CD08");
 
