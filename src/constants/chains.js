bb4490747cb599c95328b60a9f27687bc0be8cd3
demoncash08 <prodemoncoder@gmail.com>
2022-06-08 00:49:37 +0530

    Added router and some test tokens

--- src/constants/chains.js
+++ src/constants/chains.js
@@ -1,14 +1,18 @@
-export const networks = [8080 ,4 ]
+export const networks = [8080, 4];
 
 export const ChainId = {
-   MAINNET: 8080,
- 
+  MAINNET: 8080,
+
   RINKEBY: 4,
- 
-}
+};
 
 export const routerAddress = new Map();
- routerAddress.set(ChainId.MAINNET, "0xbbf7478b8be6166e099578976cb9eb702eca11ea");
+routerAddress.set(
+  ChainId.MAINNET,
+  "0xe14b41d23853c97eb9ebb6c829800ff1b11de7f8"
+);
 
-routerAddress.set(ChainId.RINKEBY, "0x14e4DAe87990FC8947718f7ac0317CdD9b58CD08");
-
+routerAddress.set(
+  ChainId.RINKEBY,
+  "0x14e4DAe87990FC8947718f7ac0317CdD9b58CD08"
+);
--- src/constants/coins.js
+++ src/constants/coins.js
@@ -1,4 +1,4 @@
-import * as chains from './chains';
+import * as chains from "./chains";
 
 // If you add coins for a new network, make sure Weth address (for the router you are using) is the first entry
 const MAINNETCoins = [
@@ -6,7 +6,7 @@ const MAINNETCoins = [
     name: "Ether",
     abbr: "ETH",
 
-    address: "", // Weth address is fetched from the router
+    address: "0xc77c720478a59a2ba135d53f3ab861c1b16efaf3", // Weth address is fetched from the router
   },
   {
     name: "Dai",
@@ -18,7 +18,37 @@ const MAINNETCoins = [
     abbr: "USDT",
     address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
   },
-]
+  {
+    name: "WazirX",
+    abbr: "WRX",
+    address: "0xbd6e8df6e2f43dcf4f78d45fcaf81ec5bb8a39e8",
+  },
+  {
+    name: "Shiba Inu",
+    abbr: "SHIB",
+    address: "0x886ad0ce8d55b3dbb4452760e2ff02162ec9ba30",
+  },
+  {
+    name: "Bitcoin",
+    abbr: "SBTC",
+    address: "0xc49f46cb20873f1171d96e7d591b1c290d06c7da",
+  },
+  {
+    name: "AAVE",
+    abbr: "AAVE",
+    address: "0xdb64742dcf15dbbfd0fda017837de0495bd0c93c",
+  },
+  {
+    name: "Basic Attention",
+    abbr: "BAT",
+    address: "0x9b17aad6261b3fb18e3bea67995392353a5c436c",
+  },
+  {
+    name: "NEXO",
+    abbr: "NEXO",
+    address: "0x6855df24832c816b45024969ec4b6f4208f6a5ca",
+  },
+];
 
 const RINKEBYCoins = [
   {
@@ -36,9 +66,7 @@ const RINKEBYCoins = [
     abbr: "USDT",
     address: "0x3b00ef435fa4fcff5c209a37d1f3dcff37c705ad",
   },
-]
-
-
+];
 
 const COINS = new Map();
 COINS.set(chains.ChainId.MAINNET, MAINNETCoins);
